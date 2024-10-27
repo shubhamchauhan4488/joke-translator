02e5cef9ffa5a75b4dacaeec1af34a433e771a50
shubhamchauhan <shubhamchauhan4488@gmail.com>
2024-10-27 14:59:54 -0400

    refactor: to typescript

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import Home from './ui/Home';
 import "./page.css";
 
-type JokeData = {
-  joke: string;
-};
+interface JokePageProps {
+  initialJoke: string;
+}
+
+async function fetchInitialJoke(): Promise<JokePageProps> {
+  const response = await fetch('https://v2.jokeapi.dev/joke/Any?type=single');
+  const data = await response.json();
+  return { initialJoke: data.joke };
+}
 
 export default async function Page() {
-  const response = await fetch('https://v2.jokeapi.dev/joke/Any?type=single', { cache: 'no-store' });
-  const data: JokeData = await response.json();
+  const data = await fetchInitialJoke();
+
   return (
     <div className='container'>
-        <Home initialJoke={data.joke} />
+        <Home initialJoke={data.initialJoke} />
     </div>
   );
 }
